848143c0d27b17a4aa695538beb996ff78dc92d9
Thibault Boursier <boursier.thibault@gmail.com>
2019-11-20 14:54:16 +0100

    remove unused import

--- components/JobList.jsx
+++ components/JobList.jsx
@@ -3,8 +3,6 @@ import { QueryRenderer } from "react-relay";
 import { getEnvironment } from "../relay/environment";
 import { LocationWithFragment } from "./Location";
 
-const graphql = require("babel-plugin-relay/macro");
-
 export const JobList = ({ locations }) => (
   <ul>
     {locations &&
